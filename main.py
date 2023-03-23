3d06fb937280705418f1dca17bd31818d4b4776b
Gabriel Lancaster-West <116901054+gxnda@users.noreply.github.com>
2023-03-23 13:59:39 +0000

    attempt at 43, dunno why its wrong

--- main.py
+++ main.py
@@ -1051,9 +1051,47 @@ def euler_42():
     #0.015993118286132812
 
 
+def euler_43():
+    """
+    Sub-string divisibility
+    Problem 43
+    The number, 1406357289, is a 0 to 9 pandigital number because it is 
+    made up of each of the digits 0 to 9 in some order, but it also has
+    a rather interesting sub-string divisibility property.
+    
+    Let d1 be the 1st digit, d2 be the 2nd digit, and so on. In this way, 
+    we note the following:
+    
+    d2d3d4=406 is divisible by 2
+    d3d4d5=063 is divisible by 3
+    d4d5d6=635 is divisible by 5
+    d5d6d7=357 is divisible by 7
+    d6d7d8=572 is divisible by 11
+    d7d8d9=728 is divisible by 13
+    d8d9d10=289 is divisible by 17
+    Find the sum of all 0 to 9 pandigital numbers with this property.
+    """
+    print("Getting permutations...")
+    all_pandigital = get_all_permutations("0123456789")
+    print("Done permutations!")
+    valid = set()
+    length = len(all_pandigital)
+    for i in range(length):
+        d = all_pandigital[i]
+        if int(d[1: 4]) % 2 == 0 and \
+            int(d[2: 5]) % 3 == 0 and \
+            int(d[3: 6]) % 5 == 0 and \
+            int(d[4: 7]) % 7 == 0 and \
+            int(d[5: 8]) % 11 == 0 and \
+            int(d[7: 10]) % 17 == 0:
+            valid.add(int(d))
+    return sum(valid)
+            
+            
+
 if __name__ == "__main__":
     start = time()
-    print(euler_42())
+    print(euler_43())
     end = time()
     print(end - start)
 
