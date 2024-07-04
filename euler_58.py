9a3c716081124832b1bb2612b88911ac75fb0f77
Gabriel Lancaster-West <gabriellancasterwest@gmail.com>
2024-07-04 22:31:25 +0100

    incorrect solution to #58

--- euler_58.py
+++ euler_58.py
@@ -1,3 +1,59 @@
+from datetime import datetime
+
+from util import is_prime
+
+
+def top_right(sl: int) -> int:
+    """
+    SL 1: 1
+    SL 3: 3, 5, 7, 9 +2
+    SL 5: 13, 17, 21, 25 +4
+    SL 7: 31, 37, 43, 49 +6
+
+    n = (SL + 1) // 2
+    SL = 2n - 1
+
+    Top right:
+       1, 3, 13, 31 -> n^2 - n + 1 ->(SL ** 2 + 3) // 4
+    """
+    return (sl ** 2 + 3) // 4
+
+
+def top_right_is_prime(sl: int) -> bool:
+    return is_prime(top_right(sl))
+
+
+def top_left(sl: int) -> int:
+    """
+    Top left:
+       1, 5, 17, 37 -> 4n^2 - 8n + 5 -> (SL - 1) ** 2 + 1
+    """
+    return (sl - 1) ** 2 + 1
+
+
+def top_left_is_prime(sl: int) -> bool:
+    return is_prime(top_left(sl))
+
+
+def bottom_left(sl: int) -> int:
+    """
+    Bottom left:
+    1, 7, 21, 43 -> 4n**2 - 6n + 3 -> SL**2 - SL + 1
+    """
+    return sl ** 2 - sl + 1
+
+
+def bottom_left_is_prime(sl: int):
+    return is_prime(bottom_left(sl))
+
+
+def bottom_right_is_prime():
+    # Bottom right:
+    #   1, 9, 25, 49 ->
+    #       1**2, 3**2, 5**2, 7**2 ->
+    #           SL**2, never prime.
+    return False
+
 
 def euler_58():
     """
@@ -18,4 +74,30 @@ def euler_58():
     If this process is continued, what is the side length of the square spiral for which the ratio of primes along
     both diagonals first falls below 10%?
     """
-    pass
+    side_length = 1
+    complete = False
+    primes, total = 0, 1
+    while not complete:
+        total += 4
+        side_length += 2
+        if top_right_is_prime(side_length):
+            primes += 1
+
+        if top_left_is_prime(side_length):
+            primes += 1
+
+        if bottom_left_is_prime(side_length):
+            primes += 1
+
+        if bottom_right_is_prime():
+            primes += 1
+
+        if (primes / total) <= 0.1:
+            return side_length
+
+
+if __name__ == "__main__":
+    start = datetime.now()
+    print(euler_58())
+    end = datetime.now()
+    print(end - start)
